7a3c982db0a530d078aa55545cb9f4ab84ebe48b
alex <alex@nabu.be>
2015-01-20 09:58:37 +0100

    Added cache

--- src/main/java/be/nabu/libs/types/MultipleDefinedTypeResolver.java
+++ src/main/java/be/nabu/libs/types/MultipleDefinedTypeResolver.java
@@ -1,6 +1,8 @@
 package be.nabu.libs.types;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import be.nabu.libs.types.api.DefinedType;
 import be.nabu.libs.types.api.DefinedTypeResolver;
@@ -9,19 +11,29 @@ public class MultipleDefinedTypeResolver implements DefinedTypeResolver {
 
 	private List<DefinedTypeResolver> resolvers;
 	
+	private Map<String, DefinedType> definedTypes = new HashMap<String, DefinedType>();
+	
 	public MultipleDefinedTypeResolver(List<DefinedTypeResolver> resolvers) {
 		this.resolvers = resolvers;
 	}
 	
 	@Override
 	public DefinedType resolve(String id) {
-		DefinedType type = null;
-		for (DefinedTypeResolver resolver : resolvers) {
-			type = resolver.resolve(id);
-			if (type != null)
-				break;
+		if (!definedTypes.containsKey(id)) {
+			synchronized(definedTypes) {
+				if (!definedTypes.containsKey(id)) {
+					DefinedType type = null;
+					for (DefinedTypeResolver resolver : resolvers) {
+						type = resolver.resolve(id);
+						if (type != null) {
+							definedTypes.put(id, type);
+							break;
+						}
+					}
+				}
+			}
 		}
-		return type;
+		return definedTypes.get(id);
 	}
 
 }
