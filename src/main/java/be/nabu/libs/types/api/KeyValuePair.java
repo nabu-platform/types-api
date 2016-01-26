7a41822d86fefc52204696080074bf1ee8d1580a
alex <alex@nabu.be>
2016-01-26 09:27:55 +0100

    Added a generic keyvalue pair

---
+++ src/main/java/be/nabu/libs/types/api/KeyValuePair.java
@@ -0,0 +1,9 @@
+package be.nabu.libs.types.api;
+
+/**
+ * This interface was added here as it is used so often in the context of types it needs to be generally accessible
+ */
+public interface KeyValuePair {
+	public String getKey();
+	public String getValue();
+}
