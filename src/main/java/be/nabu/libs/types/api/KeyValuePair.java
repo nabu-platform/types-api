1236238f0c399af4d858f2d5ac2fdfcc199876dd
alex <alex@nabu.be>
2016-02-03 12:50:11 +0100

    Added ordering

--- src/main/java/be/nabu/libs/types/api/KeyValuePair.java
+++ src/main/java/be/nabu/libs/types/api/KeyValuePair.java
@@ -1,8 +1,11 @@
 package be.nabu.libs.types.api;
 
+import javax.xml.bind.annotation.XmlType;
+
 /**
  * This interface was added here as it is used so often in the context of types it needs to be generally accessible
  */
+@XmlType(propOrder = { "key", "value" })
 public interface KeyValuePair {
 	public String getKey();
 	public String getValue();
