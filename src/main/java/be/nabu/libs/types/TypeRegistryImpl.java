d7a07d68ece4bcb933a64dcac1b2ce986f282352
alex <alex@nabu.be>
2016-03-01 09:39:35 +0100

    Added optional prioritization of includes registries

--- src/main/java/be/nabu/libs/types/TypeRegistryImpl.java
+++ src/main/java/be/nabu/libs/types/TypeRegistryImpl.java
@@ -3,7 +3,7 @@ package be.nabu.libs.types;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -16,6 +16,10 @@ import be.nabu.libs.types.api.TypeRegistry;
 
 public class TypeRegistryImpl implements ModifiableTypeRegistry {
 
+	/**
+	 * Allows you to switch whether included registries take priority over locally defined elements
+	 */
+	private boolean prioritizeIncludes;
 	private Map<String, Map<String, Element<?>>> elements = new HashMap<String, Map<String, Element<?>>>();
 	private Map<String, Map<String, ComplexType>> complexTypes = new HashMap<String, Map<String, ComplexType>>();
 	private Map<String, Map<String, SimpleType<?>>> simpleTypes = new HashMap<String, Map<String, SimpleType<?>>>();
@@ -23,9 +27,18 @@ public class TypeRegistryImpl implements ModifiableTypeRegistry {
 	
 	@Override
 	public SimpleType<?> getSimpleType(String namespace, String name) {
-		return simpleTypes.containsKey(namespace) && simpleTypes.get(namespace).containsKey(name)
-			? simpleTypes.get(namespace).get(name)
-			: getNestedSimpleType(namespace, name);
+		if (prioritizeIncludes) {
+			SimpleType<?> simpleType = getNestedSimpleType(namespace, name);
+			if (simpleType == null && simpleTypes.containsKey(namespace)) {
+				simpleType = simpleTypes.get(namespace).get(name);
+			}
+			return simpleType;
+		}
+		else {
+			return simpleTypes.containsKey(namespace) && simpleTypes.get(namespace).containsKey(name)
+				? simpleTypes.get(namespace).get(name)
+				: getNestedSimpleType(namespace, name);
+		}
 	}
 	
 	private SimpleType<?> getNestedSimpleType(String namespace, String name) {
@@ -39,9 +52,18 @@ public class TypeRegistryImpl implements ModifiableTypeRegistry {
 	
 	@Override
 	public ComplexType getComplexType(String namespace, String name) {
-		return complexTypes.containsKey(namespace) && complexTypes.get(namespace).containsKey(name)
-			? complexTypes.get(namespace).get(name)
-			: getNestedComplexType(namespace, name);
+		if (prioritizeIncludes) {
+			ComplexType complexType = getNestedComplexType(namespace, name);
+			if (complexType == null && complexTypes.containsKey(namespace)) {
+				complexType = complexTypes.get(namespace).get(name);
+			}
+			return complexType;
+		}
+		else {
+			return complexTypes.containsKey(namespace) && complexTypes.get(namespace).containsKey(name)
+				? complexTypes.get(namespace).get(name)
+				: getNestedComplexType(namespace, name);
+		}
 	}
 
 	private ComplexType getNestedComplexType(String namespace, String name) {
@@ -55,9 +77,18 @@ public class TypeRegistryImpl implements ModifiableTypeRegistry {
 	
 	@Override
 	public Element<?> getElement(String namespace, String name) {
-		return elements.containsKey(namespace) && elements.get(namespace).containsKey(name)
-			? elements.get(namespace).get(name)
-			: getNestedElement(namespace, name);
+		if (prioritizeIncludes) {
+			Element<?> element = getNestedElement(namespace, name);
+			if (element == null && elements.containsKey(namespace)) {
+				element = elements.get(namespace).get(name);
+			}
+			return element;
+		}
+		else {
+			return elements.containsKey(namespace) && elements.get(namespace).containsKey(name)
+				? elements.get(namespace).get(name)
+				: getNestedElement(namespace, name);
+		}
 	}
 	
 	private Element<?> getNestedElement(String namespace, String name) {
@@ -109,32 +140,57 @@ public class TypeRegistryImpl implements ModifiableTypeRegistry {
 
 	@Override
 	public Set<String> getNamespaces() {
-		Set<String> set = new HashSet<String>();
+		Set<String> set = new LinkedHashSet<String>();
 		set.addAll(simpleTypes.keySet());
 		set.addAll(complexTypes.keySet());
 		set.addAll(elements.keySet());
+		for (TypeRegistry nested : registries) {
+			set.addAll(nested.getNamespaces());
+		}
 		return set;
 	}
 
 	@Override
 	public List<SimpleType<?>> getSimpleTypes(String namespace) {
-		return simpleTypes.containsKey(namespace)
-			? new ArrayList<SimpleType<?>>(simpleTypes.get(namespace).values())
-			: new ArrayList<SimpleType<?>>();
+		ArrayList<SimpleType<?>> result = new ArrayList<SimpleType<?>>();
+		if (simpleTypes.containsKey(namespace)) {
+			result.addAll(simpleTypes.get(namespace).values());
+		}
+		for (TypeRegistry nested : registries) {
+			result.addAll(nested.getSimpleTypes(namespace));
+		}
+		return result;
 	}
 
 	@Override
 	public List<ComplexType> getComplexTypes(String namespace) {
-		return complexTypes.containsKey(namespace) 
-			? new ArrayList<ComplexType>(complexTypes.get(namespace).values())
-			: new ArrayList<ComplexType>();
+		ArrayList<ComplexType> result = new ArrayList<ComplexType>();
+		if (complexTypes.containsKey(namespace)) { 
+			result.addAll(complexTypes.get(namespace).values());
+		}
+		for (TypeRegistry nested : registries) {
+			result.addAll(nested.getComplexTypes(namespace));
+		}
+		return result;
 	}
 
 	@Override
 	public List<Element<?>> getElements(String namespace) {
-		return elements.containsKey(namespace) 
-			? new ArrayList<Element<?>>(elements.get(namespace).values())
-			: new ArrayList<Element<?>>();
+		ArrayList<Element<?>> result = new ArrayList<Element<?>>();
+		if (elements.containsKey(namespace)) { 
+			result.addAll(elements.get(namespace).values());
+		}
+		for (TypeRegistry nested : registries) {
+			result.addAll(nested.getElements(namespace));
+		}
+		return result;
+	}
+
+	public boolean isPrioritizeIncludes() {
+		return prioritizeIncludes;
+	}
+	public void setPrioritizeIncludes(boolean prioritizeIncludes) {
+		this.prioritizeIncludes = prioritizeIncludes;
 	}
 
 }
