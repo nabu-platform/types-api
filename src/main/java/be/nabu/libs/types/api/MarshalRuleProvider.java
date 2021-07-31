94b697eb67671ca976d42b1278808475c03bacea
Alexander Verbruggen <alex@nabu.be>
2021-07-31 09:04:01 +0200

    updated comment

--- src/main/java/be/nabu/libs/types/api/MarshalRuleProvider.java
+++ src/main/java/be/nabu/libs/types/api/MarshalRuleProvider.java
@@ -6,11 +6,13 @@ package be.nabu.libs.types.api;
  * - streams: if you marshal a stream, you read it out and it can no longer be used, if you are relaying this data to another party that's intentional, 
  * 		if however you are inspecting the pipeline, this should not be done as it disrupts the further flow of the logic
  * 		This means we set it to SOMETIMES
- * - mime content types: they tend to have circular references to one another (parent to child, child to parent), excel sheets are another example of this
+ * - mime content types: multiparts tend to have circular references to one another (parent to child, child to parent), excel sheets are another example of this
  * 		mime content types _also_ have internal streams which may suffer the stream problem from partial marshalling (suppose we detect and ignore circular references)
  * 		these are never (?) sent to a third party in their raw format, they are memory-only representation of the underlying resource
  * 		to communicate these to a third party, you will need a dedicated service to transform it into something transmittable
  * 		This means we set it to NEVER
+ * 
+ * if you currently create a new email part _with_ attachment (multipart and all), then trace over it, you will get a stackoverflow error from the xml marshaller
  * 
  * We can't simply annotate the impacted types as they can reside in third party libraries, which is why I went for the factory method
  * The default assumption is that the result is ALWAYS unless otherwise specified
