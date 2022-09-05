ff662a649882ac972f1f4636b790382bcdfce67a
Alexander Verbruggen <alex@nabu.be>
2022-09-05 10:52:59 +0200

    updated typeregistry

--- src/main/java/be/nabu/libs/types/api/TypeRegistry.java
+++ src/main/java/be/nabu/libs/types/api/TypeRegistry.java
@@ -11,4 +11,17 @@ public interface TypeRegistry {
 	public List<SimpleType<?>> getSimpleTypes(String namespace);
 	public List<ComplexType> getComplexTypes(String namespace);
 	public List<Element<?>> getElements(String namespace);
+	
+	// by default we assume the name is <namespace>.<name>
+	// the dot is not a super strong separator, but it _is_ in line with the nabu stack method of identification
+	public default Type getTypeById(String id) {
+		int index = id.lastIndexOf('.');
+		String namespace = index < 0 ? null : id.substring(0, index);
+		String name = index < 0 ? id : id.substring(index + 1);
+		SimpleType<?> simpleType = getSimpleType(namespace, name);
+		if (simpleType != null) {
+			return simpleType;
+		}
+		return getComplexType(namespace, name);
+	}
 }
