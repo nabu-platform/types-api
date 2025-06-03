be8b6ce41c57a89a6a52d69ca08fe3a537a467a3
Alexander Verbruggen <alex>
2025-06-03 18:16:58 +0200

    added seperate logic for array

--- src/main/java/be/nabu/libs/types/api/ComplexContent.java
+++ src/main/java/be/nabu/libs/types/api/ComplexContent.java
@@ -21,6 +21,7 @@ public interface ComplexContent {
 	
 	// if the value you are trying to set is null and we have to create parent instances to explicitly set that null, should we?
 	public static boolean CREATE_PARENT_FOR_NULL_VALUE = Boolean.parseBoolean(System.getProperty("be.nabu.types.createParentForNullValue", "true"));
+	public static boolean CREATE_LIST_FOR_NULL_VALUE = Boolean.parseBoolean(System.getProperty("be.nabu.types.createListForNullValue", "false"));
 	
 	public ComplexType getType();
 	public void set(String path, Object value);
